4fa1541dc4bea67f59ae663a5e619560c7402b1e
heloababwa10-dot <heloababwa10@gmail.com>
2026-05-16 12:07:43 +0700

    Update app.py

--- app.py
+++ app.py
@@ -146,5 +146,5 @@ if __name__ == '__main__':
 
     load_models()
 
-    print("🌐 Open: http://localhost:5000/upload")
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=False)
